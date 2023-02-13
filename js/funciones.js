d5a0731b3f09df9485231c653625bbd878870ec8
maxmiliano milicic <maximiliano.milicic@gmail.com>
2023-02-12 21:12:35 -0300

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").addEventListener("click", function() {
+    console.log("hola mundoooooo");
+    document.getElementById("contacto").innerHTML = "(905) 320-8700";  /** haces clin en cualquier lugar y se cambia el tecto */
+})
